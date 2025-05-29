7621edaeff1b30563b81fa8981f8162ff1d2a8e8
Viktoria Avramenko <avramenkovictoria.1402@gmail.com>
2025-05-29 12:13:53 +0300

    Initialized empty project

---
+++ src/main/java/org/docpirates/ispi/IspIApplication.java
@@ -0,0 +1,13 @@
+package org.docpirates.ispi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IspIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IspIApplication.class, args);
+    }
+
+}
