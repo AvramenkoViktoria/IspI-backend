90fdb05dffaaed2f97b72ace844fb5e842e19976
Viktoria Avramenko <avramenkovictoria.1402@gmail.com>
2025-05-31 19:36:45 +0300

    Configured database

--- src/main/java/org/docpirates/ispi/IspIApplication.java
+++ src/main/java/org/docpirates/ispi/IspIApplication.java
@@ -2,12 +2,21 @@ package org.docpirates.ispi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
-public class IspIApplication {
+public class IspIApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         SpringApplication.run(IspIApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(IspIApplication.class);
+    }
 }
+
